1f9b1d025fe18878dd86f40e979cd56896374b41
GAS <nikitashaitor@yandex.ru>
2025-08-11 10:32:38 +0300

    Исправление ошибок

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -4,8 +4,11 @@ public class GitApp {
     public static void main(String[] args) {
         System.out.println("Калькулятор: Начало");
 
+        System.out.println("Вычитание");
         int subtrackA = 10;
         int subtrackB = 20;
         int subtrackResult = subtrackB - subtrackA;
+
+        System.out.println(subtrackB + " - " + subtrackA + " = " + subtrackResult);
     }
 }
