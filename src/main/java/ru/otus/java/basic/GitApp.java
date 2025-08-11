fcd42259ed77c916698244cb92042ba34e078c30
GAS <nikitashaitor@yandex.ru>
2025-08-11 11:07:19 +0300

    task-1 ready

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -6,15 +6,17 @@ public class GitApp {
 
         System.out.println("Задача суммирования");
         int sumA = 1;
-        int sumB = 2;
+        int sumB = 3;
         int sumResult = sumA + sumB;
         System.out.println(sumA + " + " + sumB + " = " + sumResult);
 
         System.out.println("Задача вычитания");
         int subtrackA = 10;
-        int subtrackB = 20;
+        int subtrackB = 12;
         int subtrackResult = subtrackB - subtrackA;
 
         System.out.println(subtrackB + " - " + subtrackA + " = " + subtrackResult);
+
+        System.out.println("task-1");
     }
 }
