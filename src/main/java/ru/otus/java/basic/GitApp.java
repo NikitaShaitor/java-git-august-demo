1ce2aecff0ee6a70ea3b4eb011f2fd9dded1988c
GAS <nikitashaitor@yandex.ru>
2025-08-11 10:31:01 +0300

    Реализовано вычитание

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -3,5 +3,9 @@ package ru.otus.java.basic;
 public class GitApp {
     public static void main(String[] args) {
         System.out.println("Калькулятор: Начало");
+
+        int subtrackA = 10;
+        int subtrackB = 20;
+        int subtrackResult = subtrackB - subtrackA;
     }
 }
