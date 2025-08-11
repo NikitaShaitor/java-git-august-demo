1a5219b37b633068ad0ca115b60d727bfdf98488
GAS <nikitashaitor@yandex.ru>
2025-08-11 10:28:46 +0300

    Реализовано сложение переменных и вывод результата в консоль

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -3,5 +3,12 @@ package ru.otus.java.basic;
 public class GitApp {
     public static void main(String[] args) {
         System.out.println("Калькулятор: Начало");
+
+        System.out.println("Суммирование");
+        int sumA = 1;
+        int sumB = 2;
+        int sumResult = sumA + sumB;
+        System.out.println(sumA + " + " + sumB + " = " + sumResult);
+
     }
 }
